67528ba04dac079e1b5abe2c638c6ec4a3b02aa9
felipevees <juanfelipevelasquezescobar82@gmail.com>
2023-05-23 17:44:21 -0500

    limpieza de entorno de desarrollo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="container mt-4">
+        <h1 className="text-center">TECNO PUNTO</h1>
       </header>
     </div>
   );
