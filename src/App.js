bd614d59e3662ccf1b58e51e7b78559aa1b26234
felipevees <juanfelipevelasquezescobar82@gmail.com>
2023-05-24 14:44:02 -0500

    archivos de componentes Footer y Creditos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import { Creditos, Footer } from "./Componentes";
 
 function App() {
   return (
     <div className="container">
       <header className="container mt-4">
         <h1 className="text-center">TECNO PUNTO</h1>
+        <hr />
+        <Creditos />
+        <hr />
+        <Footer />
       </header>
     </div>
   );
---
+++ src/Componentes/Creditos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Creditos() {
+  return (
+    <>
+      <h1>Creditos</h1>
+    </>
+  );
+}
