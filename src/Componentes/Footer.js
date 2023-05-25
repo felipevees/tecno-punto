7b6b4984c03678ff4986fb4b23980641e9518d02
Jait Alejandro Sossa Camacho <jalex17nov@outlook.com>
2023-05-25 15:58:14 -0500

    Creditos

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-export function Footer() {
-    return (
-      <>
-        <h1>Footer</h1>    
-      </>
 
-    );
+export function Footer() { 
+  return(
+    <>
+    <h1>Footer</h1>
+    </>
+
+  
+  );
 }
